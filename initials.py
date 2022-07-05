1221e004ba30ec6a49211d7a1895afce8e3fe4e1
Lenny Ramroop <32972277+BrownieQQ@users.noreply.github.com>
2022-07-05 14:16:59 -0400

    initials.py
    -Created initials.py
    -Pulled code from my Codecademy profile

---
+++ initials.py
@@ -0,0 +1,10 @@
+#Lenny Ramroop
+#I want a job as a full stack software developer
+
+print('L     RRRR')
+print('L     R    R')
+print('L     R    R')
+print('L     RRRR')
+print('L     R R')
+print('L     R  R')
+print('LLLLL R    R')
